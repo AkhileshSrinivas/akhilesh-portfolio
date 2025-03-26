dbb1033893758d589d3be6c47ad5feef8c662f23
AkhileshSrinivas <akhileshsrinivas.nr5@gmail.com>
2025-03-26 18:57:38 +0530

    removed error

--- app/components/helper/animation-lottie.jsx
+++ app/components/helper/animation-lottie.jsx
@@ -1,20 +1,28 @@
 "use client"
 
-import Lottie from "lottie-react";
+// import Lottie from "lottie-react";
 
-const AnimationLottie = ({ animationPath, width }) => {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationPath,
-    style: {
-      width: '95%',
-    }
-  };
+// const AnimationLottie = ({ animationPath, width }) => {
+//   const defaultOptions = {
+//     loop: true,
+//     autoplay: true,
+//     animationData: animationPath,
+//     style: {
+//       width: '95%',
+//     }
+//   };
 
-  return (
-    <Lottie {...defaultOptions} />
-  );
-};
+//   return (
+//     <Lottie {...defaultOptions} />
+//   );
+// };
 
-export default AnimationLottie;
+// export default AnimationLottie;
+
+import dynamic from "next/dynamic";
+
+const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
+
+export default function AnimationLottie({ animationData }) {
+  return <Lottie animationData={animationData} loop autoplay />;
+}
